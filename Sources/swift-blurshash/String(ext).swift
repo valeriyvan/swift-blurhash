163a635ed38d90dba9b626ec9bd06779e9520d37
Valeriy Van <github@w7software.com>
2024-10-12 13:05:19 +0300

    Move String extension into separate file

---
+++ Sources/swift-blurshash/String(ext).swift
@@ -0,0 +1,41 @@
+extension String {
+    func decode83() -> Int {
+        var value: Int = 0
+        for character in self {
+            if let digit = decodeCharacters[String(character)] {
+                value = value * 83 + digit
+            }
+        }
+        return value
+    }
+}
+
+extension String {
+    subscript (offset: Int) -> Character {
+        return self[index(startIndex, offsetBy: offset)]
+    }
+
+    subscript (bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start...end]
+    }
+
+    subscript (bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start..<end]
+    }
+}
+
+private let decodeCharacters: [String: Int] = {
+    var dict: [String: Int] = [:]
+    for (index, character) in encodeCharacters.enumerated() {
+        dict[character] = index
+    }
+    return dict
+}()
+
+private let encodeCharacters: [String] = {
+    return "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~".map { String($0) }
+}()
