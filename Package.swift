a953a3c6316660a66b2bbbdfed19133057ff8f27
Valeriy Van <github@w7software.com>
2024-10-21 22:34:13 +0300

    Update monifest

--- Package.swift
+++ Package.swift
@@ -3,18 +3,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-blurshash",
+    name: "swift-blurhash",
+    platforms: [
+        .macOS("13.3"), .iOS("16.4")
+    ],
     products: [
         .library(
-            name: "swift-blurshash",
-            targets: ["swift-blurshash"]),
+            name: "Blurhash",
+            targets: ["Blurhash"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
+        //.package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.17.4")
     ],
     targets: [
         .target(
-            name: "swift-blurshash"),
+            name: "Blurhash",
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms")
+            ]
+        ),
         .testTarget(
-            name: "swift-blurshashTests",
-            dependencies: ["swift-blurshash"]
+            name: "BlurhashTests",
+            dependencies: [
+                "Blurhash",
+                //.product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ],
+            resources: [.process("Resources")] // <- `copy` or `process` doesn't really matter
         ),
     ]
 )
