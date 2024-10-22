655dd473ae762de836f98f005806604131dd2b10
Valeriy Van <github@w7software.com>
2024-10-22 18:19:52 +0300

    Get rid of suboptimal String extension

--- Sources/Blurhash/Data(ext).swift
+++ Sources/Blurhash/Data(ext).swift
@@ -21,28 +21,28 @@ public extension Data {
     }
 
     init?(blurhash blurHash: String, width: Int, height: Int, punch: Float = 1) {
-        guard blurHash.count >= 6 else { return nil }
+        let blurHashCharacters: [Character] = Array(blurHash)
 
-        let sizeFlag = String(blurHash[0]).decode83()
+        guard blurHashCharacters.count >= 6 else { return nil }
+
+        let sizeFlag = String(blurHashCharacters[0]).decode83()
         let numY = (sizeFlag / 9) + 1
         let numX = (sizeFlag % 9) + 1
 
-        let quantisedMaximumValue = String(blurHash[1]).decode83()
+        let quantisedMaximumValue = String(blurHashCharacters[1]).decode83()
         let maximumValue = Float(quantisedMaximumValue + 1) / 166
 
-        guard blurHash.count == 4 + 2 * numX * numY else { return nil }
+        guard blurHashCharacters.count == 4 + 2 * numX * numY else { return nil }
 
         let colours: [RGBFloat] = (0 ..< numX * numY).map { i in
             guard i > 0 else {
-                let value: Int = String(blurHash[2 ..< 6]).decode83()
+                let value: Int = String(blurHashCharacters[2 ..< 6]).decode83()
                 return RGBFloat(decodingDC: value)
             }
-            let value: Int = String(blurHash[4 + i * 2 ..< 4 + i * 2 + 2]).decode83()
+            let value: Int = String(blurHashCharacters[4 + i * 2 ..< 4 + i * 2 + 2]).decode83()
             return RGBFloat(decodingAC: value, maximumValue: maximumValue * punch)
         }
 
-//        let width = Int(size.width)
-//        let height = Int(size.height)
         let bytesPerRow = width * 3
         guard let data = CFDataCreateMutable(kCFAllocatorDefault, bytesPerRow * height) else { return nil }
         CFDataSetLength(data, bytesPerRow * height)
@@ -56,7 +56,8 @@ public extension Data {
 
                 for j in 0 ..< numY {
                     for i in 0 ..< numX {
-                        let basis = cos(Float.pi * Float(x) * Float(i) / Float(width)) * cos(Float.pi * Float(y) * Float(j) / Float(height))
+                        let basis = cos(Float.pi * Float(x) * Float(i) / Float(width)) *
+                            cos(Float.pi * Float(y) * Float(j) / Float(height))
                         let colour = colours[i + j * numX]
                         r += colour.r * basis
                         g += colour.g * basis
