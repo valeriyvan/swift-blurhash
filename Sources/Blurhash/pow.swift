9efe69c50c5398b0c0202a783eb91325c3bc952d
Valeriy Van <github@w7software.com>
2024-10-23 12:45:03 +0300

    Refactor pow function

--- Sources/Blurhash/pow.swift
+++ Sources/Blurhash/pow.swift
@@ -1,3 +1,23 @@
-func pow(_ base: Int, _ exponent: Int) -> Int {
-    return (0 ..< exponent).reduce(1) { value, _ in value * base }
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ^^ : PowerPrecedence
+func ^^<T: BinaryInteger> (radix: T, power: T) -> T {
+    return pow(radix, power)
+}
+
+// https://stackoverflow.com/a/39021464
+func pow<T: BinaryInteger>(_ base: T, _ power: T) -> T {
+    func expBySq(_ y: T, _ x: T, _ n: T) -> T {
+        precondition(n >= 0)
+        if n == 0 {
+            return y
+        } else if n == 1 {
+            return y * x
+        } else if n.isMultiple(of: 2) {
+            return expBySq(y, x * x, n / 2)
+        } else { // n is odd
+            return expBySq(y * x, x * x, (n - 1) / 2)
+        }
+    }
+
+    return expBySq(1, base, power)
 }
